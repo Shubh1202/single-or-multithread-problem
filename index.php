f0c33cf38f2781ea15fb2bb7b2b9f653e8c01fe0
Shubh1202 <shubhamprajapati1202@gmail.com>
2026-03-01 17:24:53 +0530

    check server latency in php

--- index.php
+++ index.php
@@ -1,11 +1,31 @@
 <?php
-echo "Hello 1\n";
-$url = "https://www.google.com";
-$data = file_get_contents($url);
-if ($data !== false) {
-    echo $data;
-} else {
-    echo "Data not found";
-}
-echo "\nHello 2";
+// echo "Hello 1\n";
+// $url = "https://www.google.com";
+// $data = file_get_contents($url);
+// if ($data !== false) {
+//     echo $data;
+// } else {
+//     echo "Data not found";
+// }
+// echo "\nHello 2";
+
+
+/***********
+ * File read operation
+ */
+// console.log() ki jagah error_log() ya echo use karein
+// Server logs mein dekhne ke liye:
+error_log("Before Async Operation");
+
+// fs.readFileSync() ka equivalent PHP mein file_get_contents() hai
+$fileContent = file_get_contents("./non-blocking-1.txt");
+
+error_log("After async operation completed");
+
+// res.writeHead(200, ...) ke liye header() function use karein
+header('Content-Type: application/json');
+http_response_code(200);
+
+// res.end(JSON.stringify(...)) ka equivalent json_encode() aur echo hai
+echo json_encode(["message" => "Server testing"]);
 ?>
